ad3b3256c4df2c46855194d6eaf558ce03b1f0dd
Ryan <jocruz@csumb.edu>
2016-10-12 10:13:14 -0700

    Added emotionRecognition module, crops images to localize faces and then uses indico api to gather facial emotion data

--- cameraCapture.py
+++ cameraCapture.py
@@ -2,44 +2,108 @@
 # 	This module should read images from the users camera and save them as image arrays.
 #Joshua Ryan Cruz
 
-import numpy as np
-import cv2 as cv 
-import tkinter as tk 
+# import numpy as np
+# import cv2 as cv 
+# import tkinter as tk 
+# import PIL.ImageTk as imTk
+import liveFeed
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+ 
+
+root = tk.Tk()
+root.bind(33, lambda e: root.quit())
+lmain = tk.Label(root)
+lmain.pack()
+width, height = 800, 600
+feed = liveFeed.liveFeed()
+feed.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+feed.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+
+def show_stream():
+	imgS = feed.stream()
+	imgtk = ImageTk.PhotoImage(image=imgS)
+	lmain.imgtk = imgtk
+	lmain.configure(image=imgtk)
+	lmain.after(100, show_stream())
 
 
 
-cv.namedWindow("preview")
-vidCamera = cv.VideoCapture(0)
-path = "C:/Users/Ryan Cruz/Documents/CSIS/CST205/Pro1Images/"
-count = 1;
-tkRoot = Tk()
-root.title = 'LiveStream'
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(100, show_frame())
 
-mainFrame = tk.Frame(root, )
+root.mainloop()
 
-def app():
-	tkRoot = Tk()
-	root.title = 'LiveStream'
-	mainFrame = tk.Frame(root, )
+# def cap_frame(flag):
+ 	
+ 	# if key == 27:
 
-if vidCamera.isOpened():
-	rval, frame = vidCamera.read()
-else:
-	rval = false
+# show_frame()
+#</escape>
+# def app():
+# 	root = tk.Tk( )
+# 	feed = liveFeed.liveFeed()
+# 	tk.Label(root, text='Welcome to Facelist', borderwidth=2).grid(row=0,column=0)
 
-while(rval):
-	cv.imshow("preview", frame)
-	rval, frame = vidCamera.read()
-	key = cv.waitKey(20)
-	if key == 32:
-		rval, capFrame = vidCamera.read()
-		cv.imwrite("cap%d.jpg"% count,capFrame)
-		count+=1
-		cv.namedWindow("lastimage")
-		cv.imshow("lastimage",capFrame) 
-	if key == 27:
-		break
-cv.destroyWindow("preview")
-#cv.imshow("lastimage",lastimage)	
+# 	image = feed.stream()
+
+# 	image = image.resize((300,300), im.BILINEAR)
+
+# 	photo = imTk.PhotoImage(img=image)
+
+# 	lbl1 = tk.Label(image=photo, anchor=NW)
+
+# 	lbl1.image = photo
+
+# 	lbl1.grid(row=0,column=1)
+
+# 	btn1 = Button()
+
+# 	btn1["text"] = "Take Picture"
+
+# 	btn1.grid(row=1, column=1)
+
+# 	root.mainloop( )
+
+
+
+	# feed = liveFeed.liveFeed()
+	# tkRoot = tk.Toplevel()
+	# tkRoot.title = 'LiveStream'
+	# img = feed.stream()
+	# lf = tk.Label(tkRoot, image = img)
+	
+	# lf.pack
+
+#app()
+
+# if vidCamera.isOpened():
+# 	rval, frame = vidCamera.read()
+# else:
+# 	rval = false
+
+# while(rval):
+# 	cv.imshow("preview", frame)
+# 	rval, frame = vidCamera.read()
+# 	key = cv.waitKey(20)
+# 	if key == 32:
+# 		rval, capFrame = vidCamera.read()
+# 		cv.imwrite("cap%d.jpg"% count,capFrame)
+# 		count+=1
+# 		cv.namedWindow("lastimage")
+# 		cv.imshow("lastimage",capFrame) 
+# 	if key == 27:
+# 		break
+# cv.destroyWindow("preview")
+# #cv.imshow("lastimage",lastimage)	
 
 
--- liveFeed.py
+++ liveFeed.py
@@ -5,47 +5,58 @@
 		Once the 'flag' is pressed, the feed will pause or wait for 5-	1o seconds and show the image that 
 		was taken. The image will be saved and then passed onward to the next module.
 """
-
 import numpy as np
 import cv2 as cv
-import Image from PIL
-import ImageTk from PIL
+import PIL.ImageTk as ImageTk
+from PIL import Image
 import time as t
-
-
+import tkinter as tk
 #class contains flad for when to pause 
-class liveFeed(cv.videoCapture):
-	def __init__(self, flag):
-		self.cam = super().__init__cv.VideoCapture(0)
-		self.flag = flag
-		self.count = 1;
+class liveFeed:
+	def __init__(self):
+		self.cam = cv.VideoCapture(0)
+		#self.flag = flag
+		self.count = 1
 
 
-#checks to see if videoCapture is opened
-def isOpen(self):
-	if cam.isOpened():
-		return true
-	else:
-		return false
+	#checks to see if videoCapture is opened
+	def isOpen(self):
+		if self.cam.isOpened():
+			return True
+		else:
+			return False
 
-#stream from cv Video object
-#returns images as tk image objects. 
-def stream(self, flag):
-	if self.isOpen():
-		running, frame = self.cam.read()
-	else: 
-		running = self.cam.open(0)
-	while(running):
-		#cv.imshow("preview", frame)
-		rval, frame = cam.read()
-		image = ImageTk.PhotoImage(frame)
-		return image
+	def set(self,propID, value):
+		self.cam.set(propID,value)
 
-#function captures a single image
-def captureImage():
+
+	#stream from cv Video object
+	#returns images as tk image objects. 
+	#lmain is a label object
+	def stream(self):
+		if self.isOpen():
+			running, frame = self.cam.read()
+		else: 
+			running = self.cam.open(0)
+		_, frame = self.cam.read()
+		frame = cv.flip(frame, 1)
+		cvimage = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)
+		img = Image.fromarray(cvimage)
+		return img
+
+	#function captures a single image
+	def captureImage():
 		rval, capFrame = self.cam.read()
-		waitKey(50)
 		success = cv.imwrite("cap%d.jpg"% count,capFrame)
 		count+=1
-		return success;
+		return (success,capFrame)
+
+	def start(self):
+		self.stream()
+
+	def stop(self): 
+		self.cam.release()
+
+
+
 	
